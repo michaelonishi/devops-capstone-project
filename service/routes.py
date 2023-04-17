9e9648dc4eb749fd643e00ec15f28fe8685615b4
Michael Onishi <4689819+michaelonishi@users.noreply.github.com>
2023-04-17 08:05:52 -0400

    implemented update account

--- service/routes.py
+++ service/routes.py
@@ -50,9 +50,7 @@ def create_accounts():
     account.deserialize(request.get_json())
     account.create()
     message = account.serialize()
-    # Uncomment once get_accounts has been implemented
-    # location_url = url_for("get_accounts", account_id=account.id, _external=True)
-    location_url = "/"  # Remove once get_accounts has been implemented
+    location_url = url_for("read_account", account_id=account.id, _external=True)
     return make_response(
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
@@ -93,7 +91,29 @@ def read_account(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-# ... place you code here to UPDATE an account ...
+@app.route("/accounts/<account_id>", methods=["PUT"])
+def update_account(account_id):
+    """
+    Updates an Account
+    This endpoint will update an Account based the data in the body that is posted
+    """
+    app.logger.info("Request to update an Account")
+    account = Account.find(account_id)
+    if not account:
+        app.logger.error("Invalid Account id: %s", account_id)
+        abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Account with id [{account_id}] could not be found.",
+        )
+
+    check_content_type("application/json")
+    account.deserialize(request.get_json())
+    account.update()
+    message = account.serialize()
+    return make_response(
+        jsonify(message), status.HTTP_200_OK
+    )
+
 
 
 ######################################################################
